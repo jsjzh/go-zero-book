4091aafb3b425e7a8aaf7cc0a9a43589c7059c3c
jinzhehao <jinzhehao@souche.com>
2022-07-27 18:32:50 +0800

    init project

---
+++ service/search/api/internal/svc/servicecontext.go
@@ -0,0 +1,20 @@
+package svc
+
+import (
+	"book/service/search/api/internal/config"
+	"book/service/search/api/internal/middleware"
+
+	"github.com/zeromicro/go-zero/rest"
+)
+
+type ServiceContext struct {
+	Config  config.Config
+	Example rest.Middleware
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config:  c,
+		Example: middleware.NewExampleMiddleware().Handle,
+	}
+}
